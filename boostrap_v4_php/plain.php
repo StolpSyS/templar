99337c45ec2dc712ea150e3dcb2b138288833fd5
jstolp <jeroenstolp@gmail.com>
2017-02-06 09:14:56 +0100

    Added BS4 plain and plain-php

---
+++ boostrap_v4_php/plain.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="description" content="INSERT_DESC">
+        <meta name="author" content="Stolp Systems <?php $y = date("Y"); echo $y; ?>">
+        <link rel="shortcut icon" href="favicon.ico">
+
+        <title>TEMPLAR::Plain-PHP v1.0.0</title>
+
+        <!-- Latest compiled and minified CSS -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+        <!-- Optional theme -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+        <!-- Latest compiled and minified JavaScript -->
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
+    </head>
+
+    <body>
+        <div class="container">
+            <h1>Hello World, this is TEMPLAR::Plain v1.0.0</h1>
+            <p>Feel free to adjust the <i>Description</i> <strong>"INSERT_DESC"</strong> (INSERT_DESC), <i>Author:</i> <strong>"Stolp Systems <?php echo $y; ?>"</strong> <i>(INSERT_AUTHOR) and Title:</i> <strong>"TEMPLAR::Plain-PHP v1.0.0"</strong> (INSERT_TITLE)</p>
+        </div>
+        
+        <script src="https://code.jquery.com/jquery-1.12.4.min.js" integrity="sha256-ZosEbRLbNQzLpnKIkEdrPv7lOy9C27hHQ+Xp8a4MxAQ=" crossorigin="anonymous"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+    </body>
+</html>
